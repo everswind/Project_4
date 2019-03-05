4302064a950db4ed23c49b1dba8e7362edd4b491
xfz-mac <everswind@gmail.com>
2019-03-04 17:03:06 -0800

    update

--- car_insurance.py
+++ car_insurance.py
@@ -19,6 +19,8 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.neighbors import KNeighborsRegressor
 from Network_backup import cv_rmse_refit_on_whole
 import statsmodels.api as sm
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import mutual_info_regression
 
 #%% write codes in main
 if __name__ == '__main__':
@@ -29,20 +31,40 @@ if __name__ == '__main__':
     kf = KFold(10, shuffle=True, random_state=42)
     reg = LinearRegression()
 
-    #%% one-hot encoding
+    #%% (1a) one-hot encoding
     cat_columns = ['ft4', 'ft5', 'ft6']
     X_onehot = pd.get_dummies(X, columns=cat_columns)
     rmse_onehot = cv_rmse_refit_on_whole(reg, X_onehot, y, kf, title='one-hot encoding', plot=True)
 
-    #%% standardize num features
+    #%% (1b) standardize num features
     num_columns = ['ft1', 'ft2', 'ft3']
     X_standard = X_onehot.copy()
     X_standard[num_columns] = StandardScaler().fit_transform(X_standard[num_columns])
     rmse_standard = cv_rmse_refit_on_whole(reg, X_standard, y, kf, title='standardization', plot=True)
 
-    #%% cut ft1, standardize ft2-3, one-hot ft4-6
+    #%% (1c) cut ft1, standardize ft2-3, one-hot ft4-6
     X_c = X_onehot.copy()
     X_c[['ft2', 'ft3']] = StandardScaler().fit_transform(X_c[['ft2', 'ft3']])
     X_c['ft1'] = pd.cut(X_c['ft1'], bins=[X_c['ft1'].min()-1, 30, 50, X_c['ft1'].max()],
                         labels=[1, 2, 3])
     rmse_c = cv_rmse_refit_on_whole(reg, X_c, y, kf, title='three transformations', plot=True)
+
+    #%% (2)
+    X_le = X.copy()
+    X_le[cat_columns] = X_le[cat_columns].apply(LabelEncoder().fit_transform)
+    f, pval = f_regression(X_le, y)
+    mi = mutual_info_regression(X_le, y)
+    corr_df = pd.DataFrame([f, pval, mi], columns=X_le.columns.values,
+                                index=['F value', 'p value', 'MI']).T
+
+    plt.scatter(X_le['ft2'], y, c=X_le['ft5'])
+    plt.xlabel('ft2')
+    plt.ylabel('charges')
+    plt.title('charges vs ft2')
+    plt.show()
+
+    plt.scatter(X_le['ft1'], y, c=X_le['ft5'])
+    plt.xlabel('ft1')
+    plt.ylabel('charges')
+    plt.title('charges vs ft1')
+    plt.show()
